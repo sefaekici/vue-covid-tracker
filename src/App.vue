e43cd2efe7e5a681a443818b029943bcd6f1261f
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-05-15 16:14:10 +0300

    Gerekli style işlemleri yapıldı.Bootstrap ve css yardımı ile ana şablon oluşturuldu.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,161 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="navbar">
+      <img src="./assets/icon-navbar.png" alt="">
+      <h2>COVİD19 TRACKER</h2>
+      <div></div>
+    </div>
+
+    <div class="selector">
+      <select name="" id="">
+        <option value="">Genaral</option>
+      </select>
+    </div>
+
+    <div class="statistics">
+      <div class="total">
+        <h5>Total</h5>
+        <p>121212</p>
+      </div>
+      <div class="recovered">
+        <h5>Recovered</h5>
+        <p>121212</p>
+      </div>
+      <div class="deaths">
+        <h5>Deaths</h5>
+        <p>12.20202</p>
+      </div>
+    </div>
+
+    <div class="table-div">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Deneme</th>
+              <th>Deneme</th>
+              <th>Deneme</th>
+            </tr>
+          </thead>
+          
+
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>deneme</td>
+              <td>deneme</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>deneme</td>
+              <td>deneme</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>deneme</td>
+              <td>deneme</td>
+            </tr>
+
+          </tbody>
+
+        </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  data() {
+    
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  body{
+    background: #eeeeee;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  #app{
+    display: flex;
+    align-items:center;
+    flex-direction: column;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .navbar{
+    width: 80%;
+    height: 100px;
+    background-color: #a8d8ea;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 1rem;
+    margin-top:1rem ;
+  }
 
-a {
-  color: #42b983;
-}
+  .navbar img{
+    margin-left: 10px;
+  }
+
+  .selector{
+    margin-top: 1rem;
+    width: 75%;
+  }
+  .selector select{
+    width: 100%;
+    height:40px;
+    border-radius: 0.5rem;
+    padding: 0px 10px;
+    outline: none;
+  }
+  .statistics{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 75%;
+    max-height: 200px;
+    border-radius: 10px;
+    margin-top: 3rem;
+    padding: 0px 50px;
+  }
+
+  .statistics p{
+    margin-top:20px;
+    font-size: 30px;
+  }
+
+  .statistics .total,.recovered,.deaths{
+    height: 150px;
+    width: 300px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  .statistics .total{
+    background:#ffffd2;
+  }
+  .statistics .recovered{
+    background: #a8d8ea;
+  }
+  .statistics .deaths{
+     background: #f29191;
+  }
+
+  .table-div{
+    width: 75%;
+    margin-top:4rem ;
+   
+  }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
 
+Vue.use(Vuetify);
 new Vue({
-  el: '#app',
+  el: "#app",
+  Vuetify,
   render: h => h(App)
-})
+});
